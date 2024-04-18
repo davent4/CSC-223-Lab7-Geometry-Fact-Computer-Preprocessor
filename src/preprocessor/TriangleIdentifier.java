e7d924753cb97df37b0aac6741a96d2d349cd374
davent4 <aventsister4@gmail.com>
2024-04-17 21:34:05 -0400

    updates to TriangleTesting
    mostly

--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -1,5 +1,6 @@
 package preprocessor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -11,6 +12,7 @@ import geometry_objects.Segment;
 import geometry_objects.Triangle;
 import geometry_objects.angle.Angle;
 import geometry_objects.angle.AngleEquivalenceClasses;
+import geometry_objects.points.Point;
 
 public class TriangleIdentifier
 {
@@ -19,6 +21,7 @@ public class TriangleIdentifier
 
 	public TriangleIdentifier(Map<Segment, Segment> segments)
 	{
+		//for each entry, key == value
 		_segments = segments;
 	}
 
@@ -37,33 +40,61 @@ public class TriangleIdentifier
 		return _triangles;
 	}
 
+	/*
+	 * adds all valid triangles to the set
+	 */
 	private void computeTriangles()
 	{
-		// make equivalenceClass of angles
-		//		each equivalenceClass has multiple LinkedEquivalenceClass
-		//		each LinkedEquivalenceClass = one equivalent angle
-		AngleIdentifier angleIden = new AngleIdentifier(_segments);		
-		AngleEquivalenceClasses aeq = angleIden.getAngles();
-	
-		// for each angle, try to mash it with two other angles
-		//		idk how the fuck to do this part
-		//		try each angle per equivalenceClass against all the other angles in all the other equivalenceClasses????
-		//		still keeps from trying same angle against equivalent angles
-		//    ok so like:
-		//		grab canonical from one linkedClass
-		//		grab the canonical from another
-		//		try it with every angle in every other linkedEquivalenceClass
-		//		then grab the second element from "another" (the second linkedList)
-		//		try IT with every angle in every other linkedEquivalenceClass
-		//		once you've made it thru every angle in "another", don't try it until you've moved onto second element in first linked class
-		//
-		Angle first = null;
-		Angle second = null;
-		Angle third = null;
-		if( 
-		// "trying it":
-		// (1) if they share the same 3 points
-				//or if they share same three rays???
-		// (2) if they DONT share a vertex
+//		//maps and sets are not indexable
+//		//whether this line of code is a good thing is yet to be seen
+//		List<Segment> segments = (List<Segment>) _segments.keySet();
+//		
+//		//tries to create a valid triangle from every combination of segments
+//		for(int i = 0; i < segments.size(); i++)
+//		{
+//			for (int j = i + 1; j < segments.size(); j++)
+//			{ 
+//				for (int k = i + 2; k < segments.size(); k++) 
+//				{ 
+//					Segment first = segments.get(i);
+//					Segment second = segments.get(j);
+//					Segment third = segments.get(k);
+//					List<Segment> segList = new ArrayList<Segment>(Arrays.asList(first, second, third));
+//					try {
+//						//if the triangle is valid, add it to the set
+//						Triangle t = new Triangle(segList);
+//						_triangles.add(t);
+//					} catch (FactException e) {
+//						//if an exception is caught, then the triangle is not valid
+//						//simply move onto a new combo of segments
+//					}
+//				}
+//			}
+//		}
+
+		//maps and sets are not indexable
+		//whether this line of code is a good thing is yet to be seen
+		Set<Segment> segments = _segments.keySet();
+
+		//tries to create a valid triangle from every combination of segments
+		for(Segment first : segments)
+		{
+			for (Segment second : segments)
+			{ 
+				for (Segment third : segments) 
+				{ 
+					List<Segment> segList = new ArrayList<Segment>(Arrays.asList(first, second, third));
+					try {
+						//if the triangle is valid, add it to the set
+						Triangle t = new Triangle(segList);
+						_triangles.add(t);
+					} catch (FactException e) {
+						//if an exception is caught, then the triangle is not valid
+						//simply move onto a new combo of segments
+					}
+				}
+			}
+		}
 	}
 }
+
